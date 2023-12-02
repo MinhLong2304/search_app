2c4eeefed6c668205d439bec303811c3d836a493
minhlong2304 <tkcualong9881@gmail.com>
2023-12-02 12:14:17 +0700

    add body section

---
+++ src/app/components/SearchBar.jsx
@@ -0,0 +1,55 @@
+"use client";
+import React, { useState } from 'react'
+import { CiSearch } from "react-icons/ci";
+import { FaMicrophone } from "react-icons/fa";
+import { useRouter } from 'next/navigation';
+
+export default function SearchBar() {
+  const [input,setInput]=useState("");
+  const router=useRouter();
+  const [randomSearchLoading, setRandomSeachLoading]=useState(false);
+  function handleSubmit(e){
+    e.preventDefault();
+    if(!input.trim()) return;
+    router.push(`/search/web?searchTerm=${input}`);
+
+
+  }
+  async function randomSearch() {
+    setRandomSeachLoading(true);
+    const response = await fetch("https://random-word-api.herokuapp.com/word")
+        .then((res) => res.json())
+        .then((data) => data[0]);
+    if (!response) return;
+    router.push(`/search/web?searchTerm=${response}`);
+    setRandomSeachLoading(false);
+}
+
+  return (
+    <>
+    <form onSubmit={handleSubmit} className='flex w-full  mt-5 mx-auto max-w-[90%] px-5 py-3 border border-gray-200 rounded-full hover:shadow-md transition-shadow focus-within:shadow-md sm:max-w-xl lg:max-w-2xl'>
+    <CiSearch className='text-xl text-gray-500 mr-3' />
+    <input 
+      type="text"
+      className='flex-grow focus:outline-none' 
+      onChange={(e)=> setInput(e.target.value)}
+      value={input}
+      />
+    <FaMicrophone className='text-lg'/>
+
+
+
+    </form>
+    <div className='flex flex-col space-y-2 sm:space-y-0 sm:space-x-4 justify-center sm:flex-row mt-8'>
+      <button onClick={handleSubmit} className='btn'>Search button</button>
+      <button disabled={randomSearchLoading} onClick={randomSearch} className='btn flex items-center justify-center disabled:opacity-80'>
+        {randomSearchLoading? (
+          <img src="spinner.svg" alt='loading...' className='h-6 text-center'/>)
+         : ("I am feeling lucky") }
+        
+        </button>
+
+    </div>
+    </>
+  )
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,24 @@
+import Image from 'next/image';
 import HomeHeader from './/components/HomeHeader';
+import SearchBar from './components/SearchBar';
 
 export default function Home() {
   return (
+    <>
+       {/*Header */}
     <HomeHeader></HomeHeader>
+    
+    {/*Body*/}
+    <div className='flex flex-col items-center mt-24'>
+      <Image
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1920px-Google_2015_logo.svg.png"
+        width={300}
+        height={100}
+      />
+      <SearchBar></SearchBar>
+    </div>
+    
+    
+    </>
   )
 }
