3a32c53598a6cde38c961406e42e60c88c411478
minhlong2304 <tkcualong9881@gmail.com>
2023-12-03 01:16:12 +0700

    finish footer location detect

--- src/app/components/ContryLookup.jsx
+++ src/app/components/ContryLookup.jsx
@@ -3,10 +3,14 @@
 import React, { useEffect, useState } from 'react'
 
 export default function ContryLookup() {
-  const[country,setCountry]= useState("United States")
-  useEffect(()=>{
-    fetch(`https://extreme-ip-lookup.com/json/?key=${process.env.IP_API_KEY}`).then((res) => res.json()).then((data) => setCountry(data.country));  
-  },[]);
+  const[country,setCountry]= useState("")
+  useEffect(() => {
+    fetch(`https://extreme-ip-lookup.com/json/?key=${process.env.NEXT_PUBLIC_IP_API_KEY}`)
+      .then((res) => res.json())
+      .then((data) => setCountry(data.country))
+      .catch((error) => console.error("Error fetching country:", error));
+  }, []);
+  
     return (
     <div>{country}</div>
   )
