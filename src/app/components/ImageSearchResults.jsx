32cc4f782b48cce946455165ca448f7524a5bef7
minhlong2304 <tkcualong9881@gmail.com>
2023-12-03 20:28:12 +0700

    add image result

---
+++ src/app/components/ImageSearchResults.jsx
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function ImageSearchResults({results}) {
+    //console.log(results);
+  return (
+    
+    <div className='sm:pb-24 pb-40  mt-4'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-3 space-x-4'>
+        {results.items.map((result) => (
+            <div className="mb-8" key={result.link}>
+                <div className='group'>
+                    <Link className='' href={result}>
+                        <img src={result.link} alt={result.title} className='h-60 group-hover:shadow-xl w-full object-contain transition-shadow'/>
+                    </Link>
+                    <Link href={result}>
+                        <h2 className='group-hover:underline truncate text-xl'>{result.title}</h2>
+                    </Link>
+                    <Link href={result}>
+                        <p className='group-hover:underline text-gray-600 '>{result.displayLink}</p>
+                    </Link>
+                </div>
+            </div>
+            
+        ))
+        }
+        </div>
+        
+    </div>
+  )
+}
