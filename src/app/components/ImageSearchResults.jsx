1cb39336b07c076242df3136474ea35bf57d00b4
minhlong2304 <tkcualong9881@gmail.com>
2023-12-03 21:45:27 +0700

    add PagninationButtons components

--- src/app/components/ImageSearchResults.jsx
+++ src/app/components/ImageSearchResults.jsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
+import PaginationButtons from './PaginationButtons'
 
 export default function ImageSearchResults({results}) {
     //console.log(results);
@@ -24,8 +25,9 @@ export default function ImageSearchResults({results}) {
             
         ))
         }
-        </div>
         
+        </div>
+        <PaginationButtons/> 
     </div>
   )
 }
