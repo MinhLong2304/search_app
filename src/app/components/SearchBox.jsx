0d58699180fdb66b776254125cce5800c2402bcb
minhlong2304 <tkcualong9881@gmail.com>
2023-12-03 10:55:32 +0700

    complete search box component

--- src/app/components/SearchBox.jsx
+++ src/app/components/SearchBox.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+"use client"
+import { useSearchParams,useRouter } from 'next/navigation';
+import React, { useState } from 'react'
+import { RxCross2 } from "react-icons/rx";
+import { FaMicrophone } from "react-icons/fa6";
+import { CiSearch } from "react-icons/ci";
 
 export default function SearchBox() {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const searchTerm = searchParams.get("searchTerm");
+  const [term, setTerm]= useState(searchTerm || "");
+  function handleSubmit(e){
+    e.preventDefault();
+    if(!term) return;
+    router.push(`/search/web?searchTerm=${term}`);
+  }
   return (
-    <div>SearchBox</div>
+    <form className='flex border border-gray-200 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl items-center' onSubmit={handleSubmit}>
+      <input type='text' value={term} onChange={(e) => setTerm(e.target.value)} className='w-full focus:outline-none'></input>
+      <RxCross2 className='text-2xl text-gray-500 cursor-pointer sm:mr-2' onClick={() => setTerm("")} />
+      <FaMicrophone className='sm:inline-flex hidden text-4xl text-blue-500 pl-4 border-l-2 border-gray-300 mr-3' />
+      <CiSearch className='text-2xl hidden sm:inline-flex text-blue-500 cursor-pointer' onClick={handleSubmit}/>
+
+
+      </form>
   )
 }
